6f1a537c8d480c42d4b239e972c29320a66e4070
Matheus Lucas <matheus.lucas1105@gmail.com>
2023-11-06 16:09:06 -0300

    Withdraw

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -104,7 +104,8 @@ public class TrybankLib
     // 6. Construa a funcionalidade de sacar dinheiro
     public void Withdraw(int value)
     {
-        throw new NotImplementedException();
+        if (!Logged) throw new AccessViolationException("Usuário não está logado");
+        Bank[loggedUser, 3] -= value;
     }
 
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
